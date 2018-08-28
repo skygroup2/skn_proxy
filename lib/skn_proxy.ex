2ca4d18e805eaad14d15d840a72cf104a73dec94
king22 <henrytan.next@gmail.com>
2018-08-29 05:17:52 +0700

    add web hook

--- lib/skn_proxy.ex
+++ lib/skn_proxy.ex
@@ -1,3 +1,11 @@
 defmodule Skn.Proxy.Sup do
-
+  def start_proxy_hook() do
+    http_port = Skn.DB.Config.get(:web_proxy_port, nil)
+    if is_integer(http_port) and http_port > 1024 and http_port < 65535 do
+      dispatch = :cowboy_router.compile([{:_, [{:_, Skn.Proxy.RestApi, []}]}])
+      :cowboy.start_clear(:http, [{:port, http_port}], [{:env, [{:dispatch, dispatch}]}])
+    else
+      {:ok, :ignore}
+    end
+  end
 end
