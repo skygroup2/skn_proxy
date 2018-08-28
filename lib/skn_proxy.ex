6016b1257a5f1ebd89c82e2f21323e9d62bc19da
king22 <henrytan.next@gmail.com>
2018-08-29 05:22:10 +0700

    Fix some bug relate to config

--- lib/skn_proxy.ex
+++ lib/skn_proxy.ex
@@ -1,6 +1,6 @@
 defmodule Skn.Proxy.Sup do
   def start_proxy_hook() do
-    http_port = Skn.DB.Config.get(:web_proxy_port, nil)
+    http_port = Skn.Config.get(:web_proxy_port, nil)
     if is_integer(http_port) and http_port > 1024 and http_port < 65535 do
       dispatch = :cowboy_router.compile([{:_, [{:_, Skn.Proxy.RestApi, []}]}])
       :cowboy.start_clear(:http, [{:port, http_port}], [{:env, [{:dispatch, dispatch}]}])
