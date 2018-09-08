182f0cfbf06217d41cca05961bbae3ac1578ab70
king22 <henrytan.next@gmail.com>
2018-09-08 07:41:44 +0700

    support Hulk

---
+++ lib/proxy_hulk.ex
@@ -0,0 +1,99 @@
+defmodule ProxyHulk do
+  use GenServer
+  require Logger
+  import Skn.Util, only: [
+    reset_timer: 3,
+    check_ipv4: 1
+  ]
+  @name :proxy_hulk
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], name: @name)
+  end
+
+  def init(_name) do
+    Process.flag(:trap_exit, true)
+    reset_timer(:check_tick_ref, :check_tick, 20000)
+    {:ok, %{queue: :queue.new()}}
+  end
+
+  def handle_call(request, from, state) do
+    Logger.error "drop unknown #{inspect request} from #{inspect from}"
+    {:reply, {:error, :badreq}, state}
+  end
+
+  def handle_cast(cast, state) do
+    Logger.error "drop unknown #{inspect cast}"
+    {:noreply, state}
+  end
+
+  def handle_info(:check_tick, state) do
+    try do
+      case Skn.Config.get(:proxy_hulk) do
+        {username, _} ->
+          n = grab_proxy(username) |> import_hulk()
+          Logger.debug("imported #{n} proxies")
+          ProxyGroup.update_static()
+        _ ->
+          :ok
+      end
+    catch
+      _, exp ->
+        Logger.error("update failed #{inspect exp}, #{inspect System.stacktrace()}")
+    end
+    reset_timer(:check_tick_ref, :check_tick, Skn.Config.get(:tm_proxy_hulk, 120_000))
+    {:noreply, state}
+  end
+
+  def handle_info(info, state) do
+    Logger.error "drop unknown #{inspect info}"
+    {:noreply, state}
+  end
+
+  def code_change(_vsn, state, _extra) do
+    {:ok, state}
+  end
+
+  def terminate(reason, _state) do
+    Logger.debug "stopped by #{inspect reason}"
+    :ok
+  end
+
+  def grab_proxy(username) do
+    {:ok, x} = HTTPoison.get "https://www.proxyhulk.com/list/getmylist.php?type=socks&user=#{username}"
+
+    <<239, 187, 191, body :: binary>> = x.body
+    res = String.replace(body, "<br />", "")
+    res2 = String.split(res, "\n")
+    res3 = Enum.map res2, fn (x) ->
+      case String.split(x, ":") do
+        [x, y] -> {x, String.to_integer(y)}
+        _ -> nil
+      end
+    end
+    res4 = Enum.map res3, fn x ->
+      case x do
+        {ip, port} ->
+          case check_ipv4(ip) do
+            {true, :public} ->
+              {:ok, addr} = :inet.parse_ipv4_address(:erlang.binary_to_list(ip))
+              %{proxy: {:socks5, addr, port}, ip: ip}
+            _ ->
+              nil
+          end
+        _ ->
+          nil
+      end
+    end
+    Enum.filter res4, fn x -> is_map(x) end
+  end
+
+  def import_hulk(proxies) do
+    Enum.each proxies, fn x ->
+      %{proxy: proxy, ip: ip} = x
+      r = %{id: {proxy, nil}, ip: ip, tag: :static, assign: :hulk, info: %{failed: 0}}
+      Skn.DB.ProxyList.write(r)
+    end
+    length(proxies)
+  end
+end
