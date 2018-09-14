ffebefc5bdbe6c8476ff6dd126cd0fd712dc0717
king22 <henrytan.next@gmail.com>
2018-09-14 15:40:28 +0700

    periodictly update static

--- lib/proxy_group.ex
+++ lib/proxy_group.ex
@@ -4,6 +4,9 @@ defmodule ProxyGroup do
   """
   use GenServer
   require Logger
+  import Skn.Util, only: [
+    reset_timer: 3
+  ]
 
   @country_list [:ae , :af , :ag , :ai , :al , :am , :an , :ao , :aq , :ar , :as , :at , :au , :aw , :ax , :az , :ba , :bb , :bd , :be , :bf , :bg , :bh , :bi , :bj , :bl , :bm , :bn , :bo , :br , :bs , :bt , :bv , :bw , :by , :bz , :ca , :cc , :cd , :cf , :cg , :ch , :ci , :ck , :cl , :cm , :cn , :co , :cr , :cu , :cv , :cx , :cy , :cz , :de , :dj , :dk , :dm , :do , :dz , :ec , :ee , :eg , :eh , :er , :es , :et , :fi , :fj , :fk , :fm , :fo , :fr , :ga , :gb , :gd , :ge , :gf , :gg , :gh , :gi , :gl , :gm , :gn , :gp , :gq , :gr , :gs , :gt , :gu , :gw , :gy , :hk , :hm , :hn , :hr , :ht , :hu , :id , :ie , :il , :im , :in , :io , :iq , :ir , :is , :it , :je , :jm , :jo , :jp , :ke , :kg , :kh , :ki , :km , :kn , :kp , :kr , :kw , :ky , :kz , :la , :lb , :lc , :li , :lk , :lr , :ls , :lt , :lu , :lv , :ly , :ma , :mc , :md , :me , :mf , :mg , :mh , :mk , :ml , :mm , :mn , :mo , :mp , :mq , :mr , :ms , :mt , :mu , :mv , :mw , :mx , :my , :mz , :na , :nc , :ne , :nf , :ng , :ni , :nl , :no , :np , :nr , :nu , :nz , :om , :pa , :pe , :pf , :pg , :ph , :pk , :pl , :pm , :pn , :pr , :ps , :pt , :pw , :py , :qa , :re , :ro , :rs , :ru , :rw , :sa , :sb , :sc , :sd , :se , :sg , :sh , :si , :sj , :sk , :sl , :sm , :sn , :so , :sr , :ss , :st , :sv , :sy , :sz , :tc , :td , :tf , :tg , :th , :tj , :tk , :tl , :tm , :tn , :to , :tr , :tt , :tv , :tw , :tz , :ua , :ug , :um , :us , :uy , :uz , :va , :vc , :ve , :vg , :vi , :vn , :vu , :wf , :ws , :ye , :yt , :za , :zm , :zw]
   @country_black_list ["ai" , "an" , "ao" , "aq" , "as" , "bl" , "bv" , "cc" , "cf" , "cg" , "ck" , "cu" , "cx" , "dj" , "eh" , "er" , "fk" , "gs" , "gw" , "hm" , "io" , "ki" , "km" , "kp" , "kr" , "kz" , "li" , "ls" , "mf" , "mh" , "ms" , "mw" , "nf" , "nr" , "nu" , "pn" , "pw" , "sb" , "sc" , "sh" , "sj" , "sl" , "sm" , "ss" , "td" , "tf" , "tk" , "tl" , "to" , "tv" , "um" , "va" , "vg" , "vu" , "wf" , "ye" , "xk" , "cn" , "ru" , "tw" , "hk" , "il" , "dz" , "eg" , "id" , "in" , "ir" , "ma" , "pk" , "ps" , "sy" , "th" , "uy" , "tn" , "sa" , "sn" , "bi" , "st" , "gn" , "pg" , "ws" , "ne" , "mp" , "sz" , "lc" , "fm" , "rw" , "bf" , "cd" , "tm" , "lr" , "mr" , "cm" , "et" , "yt" , "az" , "so" , "pm" , "tg" , "vn" , "my" , "ua" , "tr" , "ec" , "do" , "om" , "vi" , "sx" , "kh" , "dm" , "ag" , "bt" , "mc" , "af" , "cv" , "gq" , "bw" , "bd" , "ga" , "gm" , "ml"]
@@ -331,6 +334,8 @@ defmodule ProxyGroup do
   def init({:static, group}) do
     Process.flag(:trap_exit, true)
     send(self(), {:update, group})
+    tm_proxy_sync = Skn.Config.get(:tm_proxy_sync, 1200_000)
+    reset_timer(:update_static_ref, :update_static, tm_proxy_sync)
     {:ok, %{id: :static, group: [], ptr: :rand.uniform(5000), cc: %{}}}
   end
 
@@ -481,6 +486,13 @@ defmodule ProxyGroup do
   def handle_cast(request, state) do
     Logger.warn("drop unknown cast #{inspect(request)}")
     {:noreply, state}
+  end
+
+  def handle_info(:update_static, state) do
+    proxies = Skn.DB.ProxyList.list_tag(:static)
+    tm_proxy_sync = Skn.Config.get(:tm_proxy_sync, 1200_000)
+    reset_timer(:update_static_ref, :update_static, tm_proxy_sync)
+    handle_cast({:update, proxies}, state)
   end
 
   def handle_info({:update, group}, state) do
