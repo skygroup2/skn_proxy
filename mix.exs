2f6b58aeb59aee2b03b63f00c3878f005b5525f9
king22 <henrytan.next@gmail.com>
2019-03-20 10:28:31 +0700

    update skn_proxy

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule SknProxy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Skn.Proxy, []},
       extra_applications: [:logger]
     ]
   end
