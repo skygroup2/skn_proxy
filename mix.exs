354d48877ff72ee861cfb2f5b0b0ee76be68b271
king22 <henrytan.next@gmail.com>
2020-04-10 16:06:59 +0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -30,15 +30,15 @@ defmodule SknProxy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:logger_lager_backend, git: "https://github.com/gskynet/logger_lager_backend.git", branch: "master"},
+      {:logger_lager_backend, git: "https://github.com/Subhuti20/logger_lager_backend.git", branch: "master"},
       {:skn_lib, git: "git@gitlab.com:gskynet_lib/skn_lib.git", branch: "master"},
       {:skn_proto, git: "git@gitlab.com:gskynet_lib/skn_proto.git", branch: "master"},
       {:skn_bus, git: "git@gitlab.com:gskynet_lib/skn_bus.git", branch: "master"},
       {:lager, "~> 3.8", override: true},
       {:ranch, "~> 1.7", override: true},
       {:cowboy, "~> 2.6"},
-      {:ecto, "~> 3.2"},
-      {:ecto_sql, "~> 3.2"},
+      {:ecto, "~> 3.4"},
+      {:ecto_sql, "~> 3.4"},
       {:postgrex, "~> 0.15"}
     ]
   end
